a60c868471ae84a90357015547c82e8e927ced5d
Mertcan <98522906+fr0sty-m@users.noreply.github.com>
2024-08-07 11:57:15 +0300

    Update autocomplete.lua

--- lua/fr0sty/plugins/autocomplete.lua
+++ lua/fr0sty/plugins/autocomplete.lua
@@ -31,6 +31,8 @@ return {
 			--  into multiple repos for maintenance purposes.
 			"hrsh7th/cmp-nvim-lsp",
 			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-cmdline",
 		},
 		config = function()
 			-- See `:help cmp`
@@ -107,6 +109,7 @@ return {
 					{ name = "nvim_lsp" },
 					{ name = "luasnip" },
 					{ name = "path" },
+					{ name = "buffer" },
 				},
 			})
 		end,
